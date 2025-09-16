8221819475b9354a83f850e5cea1ca0f1c9b8308
Mitesh Agrawal <miteshagrawal972@gmail.com>
2025-09-16 19:22:17 +0530

    init

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+require("dotenv/config")
 const app = express();
 
 app.get("/", (req,res) => {
   res.send("Hi ther")
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT
+app.listen(PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
